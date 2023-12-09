45dcd8e3c7bfcb8d89570eae9a10e8a46edf403a
lukaszdutka <lukaszdutka2@gmail.com>
2023-12-09 18:19:41 +0100

    day 8 solution 2 refactor

--- src/main/kotlin/day_009/Main.kt
+++ src/main/kotlin/day_009/Main.kt
@@ -2,7 +2,6 @@ package day_009
 
 import java.io.File
 
-
 private const val inputPath =
     "/Users/lukaszdutka/IdeaProjects/fun/advent-of-code-2023/src/main/kotlin/day_009/input.txt"
 //    "/Users/lukaszdutka/IdeaProjects/fun/advent-of-code-2023/src/main/kotlin/day_009/input_small.txt"
@@ -18,33 +17,18 @@ fun solutionV2(lines: List<String>) {
     println(lines.sumOf(::solveV2))
 }
 
-fun solveV2(line: String): Int {
-    val startingInts = line.split(" ").filter { it.isNotBlank() }.map { it.toInt() }.reversed()
-    var ints = startingInts
-    var pairs = ints.zipWithNext()
-
-    ints = mutableListOf()
+fun solveV2(lineString: String): Int {
+    var line = lineString.split(" ").map { it.toInt() }.reversed()
     val lasts = mutableListOf<Int>()
-    var shouldContinue = true
-    lasts.add(startingInts.last())
-    while (shouldContinue) {
-        for (pair in pairs) {
-            ints.add(pair.first - pair.second)
-        }
-        pairs = ints.zipWithNext()
-        lasts.add(ints[ints.size - 1])
-        if (ints.all { it == 0 }) {
-            shouldContinue = false
-        }
-        ints.clear()
-    }
 
-    val realLasts = mutableListOf<Int>()
-    for (last in lasts.reversed()) {
-        val x = last - (realLasts.lastOrNull() ?: 0)
-        realLasts.add(x)
+    while (true) {
+        lasts.add(line.last())
+        line = line.zipWithNext { first, second -> first - second }
+        if (line.all { it == 0 }) {
+            break
+        }
     }
-    return realLasts.last()
+    return lasts.reversed().reduce { acc, x -> x - acc }
 }
 
 fun solutionV1(lines: List<String>) {
