46ec48c6393699799b43aa2c877529c950ce6fd3
lukaszdutka <lukaszdutka2@gmail.com>
2023-12-11 21:21:17 +0100

    day11 cleaning

--- src/main/kotlin/day_011/Main.kt
+++ src/main/kotlin/day_011/Main.kt
@@ -1,7 +1,6 @@
 package day_011
 
 import java.io.File
-import kotlin.math.abs
 import kotlin.math.max
 import kotlin.math.min
 
@@ -17,106 +16,45 @@ fun main() {
     for ((i, line) in lines.withIndex()) {
         array[i] = line.toCharArray()
     }
-    solutionV1(array)
-    solutionV2(array)
+    solutionV2(array, xValue = 2)
+    solutionV2(array, xValue = 1_000_000)
 }
 
-fun solutionV1(array: Array<CharArray>) {
-    println("Solution V1")
+fun solutionV2(array: Array<CharArray>, xValue: Int) {
+    println("Solution for xValue=$xValue")
     val galaxyMap = mutableListOf<MutableList<Char>>()
 
     for (row in array) {
-        if (row.all { it == '.' }) {
-            galaxyMap.add(row.toMutableList())
-        }
-        galaxyMap.add(row.toMutableList()) // góra-dół
-    }
-    for (x in array[0].indices.reversed()) {
-        if (galaxyMap.all { (it[x] == '.') }) {
-            for (index2 in galaxyMap.indices) {
-                galaxyMap[index2].add(x, '.')
-            }
-        }
-    }
-
-    val allGalaxies = mutableListOf<Pair<Int, Int>>()
-    for ((y, row) in galaxyMap.withIndex()) {
-        for ((x, char) in row.withIndex()) {
-            if (char == '#') {
-                allGalaxies.add(Pair(y, x))
-            }
-        }
-    }
-
-    var distance = 0
-    for (i in allGalaxies.indices) {
-        for (j in i..<allGalaxies.size) {
-            val g1 = allGalaxies[i]
-            val g2 = allGalaxies[j]
-
-            distance += abs(g1.first - g2.first) + abs(g1.second - g2.second)
-        }
-    }
-    println(distance)
-}
-
-fun solutionV2(array: Array<CharArray>) {
-    println("Solution V2")
-    val galaxyMap = mutableListOf<MutableList<Char>>()
-    val xValue = 1_000_000
-
-    for (row in array) {
-        if (row.all { it == '.' || it == 'x' }) {
+        if (row.all { it != '#' }) {
             galaxyMap.add(row.toMutableList().map { 'x' }.toMutableList())
         } else {
             galaxyMap.add(row.toMutableList()) // góra-dół
         }
     }
     for (x in array[0].indices.reversed()) {
-        if (galaxyMap.all { (it[x] == '.' || it[x] == 'x') }) {
+        if (galaxyMap.all { (it[x] != '#') }) {
             for (index2 in galaxyMap.indices) {
                 galaxyMap[index2][x] = 'x'
             }
         }
     }
 
-    val allGalaxies = mutableListOf<Pair<Int, Int>>()
-    for ((y, row) in galaxyMap.withIndex()) {
-        for ((x, char) in row.withIndex()) {
-            if (char == '#') {
-                allGalaxies.add(Pair(y, x))
-            }
-        }
-    }
+    val allGalaxies = galaxyMap.flatMapIndexed { y, row ->
+        row.mapIndexedNotNull { x, char -> if (char == '#') Pair(y, x) else null }
+    }.toList()
 
     var distance = 0L
     for (i in allGalaxies.indices) {
         for (j in i..<allGalaxies.size) {
             val g1 = allGalaxies[i]
             val g2 = allGalaxies[j]
-            if (g1 == g2) {
-                continue
-            }
 
-            var tempDistance = 0
             for (y in min(g1.first, g2.first).rangeUntil(max(g1.first, g2.first))) {
-                if (galaxyMap[y][g1.second] == '.' || galaxyMap[y][g1.second] == '#') {
-                    tempDistance += 1
-                } else {
-                    tempDistance += xValue
-                }
+                distance += (if (galaxyMap[y][g1.second] != 'x') 1 else xValue)
             }
             for (x in min(g1.second, g2.second).rangeUntil(max(g1.second, g2.second))) {
-                if (galaxyMap[g2.first][x] == '.' || galaxyMap[g2.first][x] == '#') {
-                    tempDistance += 1
-                } else {
-                    tempDistance += xValue
-                }
+                distance += (if (galaxyMap[g2.first][x] != 'x') 1 else xValue)
             }
-            println("total temp distance=$tempDistance")
-            distance += tempDistance.toLong()
-            //exp: 374
-            //exp: 9370588
         }
     }
     println(distance)
