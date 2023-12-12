540c1c71425a685922fde6f031557c333e49dd34
lukaszdutka <lukaszdutka2@gmail.com>
2023-12-12 16:04:30 +0100

    all but one case works

--- src/main/kotlin/day_012/Main.kt
+++ src/main/kotlin/day_012/Main.kt
@@ -10,11 +10,11 @@ private const val inputPath =
 fun main() {
     val lines = File(inputPath).readLines()
 
-    myCheck(calculatePaper("???.### 1,1,3"), 1, "first case failed")
-    myCheck(calculatePaper(".??..??...?##. 1,1,3"), 4, "second case failed")
-    myCheck(calculatePaper("?#?#?#?#?#?#?#? 1,3,1,6"), 1, "third case failed")
-    myCheck(calculatePaper("????.######..#####. 1,6,5"), 4, "fourth case failed")
-    myCheck(calculatePaper("?###???????? 3,2,1"), 10, "fifth case failed")
+//    myCheck("???.### 1,1,3", 1, "first case failed")
+    myCheck(".??..??...?##. 1,1,3", 4, "second case failed")
+//    myCheck("?#?#?#?#?#?#?#? 1,3,1,6", 1, "third case failed")
+//    myCheck("????.######..#####. 1,6,5", 4, "fourth case failed")
+//    myCheck("?###???????? 3,2,1", 10, "fifth case failed")
 //    solutionV1(lines)
     val linesV2 = lines.map { v2Lines(it) }
 //    solutionV1(linesV2)
@@ -22,9 +22,14 @@ fun main() {
 //    solutionBetter(linesV2)
 }
 
-fun myCheck(calculated: Int, expected: Int, message: String) {
+fun myCheck(value: String, expected: Int, message: String) {
+    val calculated = calculatePaper(value)
     if (calculated != expected) {
-        println("$message => expected=$expected, but got calculated=$calculated")
+        println("For string: $value")
+        println("FAIL $message => expected=$expected, but got calculated=$calculated")
+    } else {
+        println("For string: $value")
+        println("SUCCESS => expected == calculated ($expected)")
     }
 }
 
@@ -40,28 +45,38 @@ fun calculatePaper(line: String): Int {
 //    val d = IntArray(dTemp.size + 1)
 //    d[0] = 0
 //    dTemp.copyInto(d, 1)
-    val d = calculateDescriptions(splitted)
-    val solvedLine = calculateSolvedLine(splitted) //partial colouring, if not everythink known
+    val dFirst = calculateDescriptions(splitted)
+    val solvedLineFirst = calculateSolvedLine(splitted) //partial colouring, if not everythink known
+    val d = IntArray(dFirst.size + 1)
+    dFirst.copyInto(d, 1)
+    d[0] = -99
+    val solvedLine = IntArray(solvedLineFirst.size + 1)
+    solvedLineFirst.copyInto(solvedLine, 1)
+    solvedLine[0] = -99
+
     // n - length of line
     // k - number of blocks of black cells (d.size)
-    val n = solvedLine.size
-    val k = d.size
+    val n = solvedLine.size - 1
+    val k = d.size - 1
 
-    // TODO: init properly
-    //is sol == dp?
     val sol = Array(n + 1) { IntArray(k + 1) { -1 } }
 
     // d[j] -> j'ty element d/description
     // 1>1(j) "returns 1 if to the left of block j there must be at least one white cell"
     // (?) - usually 1, or not if it's start of array
 
-    sol[0][0] = 1
+//    sol[0][0] = -1
     val solve = solve(n, k, sol, d, solvedLine)
-    println(solvedLine.joinToString(separator = ""))
+//    println(
+//        solvedLine.joinToString(separator = "")
+//            .replace("-1", "?")
+//            .replace("0", ".")
+//            .replace("1", "#")
+//    )
     return solve
 }
 
-private fun atLeastOneWhiteCell(j: Int): Int = if (j == 0) 0 else 1
+private fun atLeastOneWhiteCell(j: Int): Int = if (j == 1) 0 else 1
 
 private fun calculateDescriptions(splitted: List<String>) =
     splitted[1].split(",").map { it.toInt() }.toIntArray()
@@ -80,16 +95,20 @@ private fun calculateSolvedLine(splitted: List<String>) = splitted[0].split("").
 fun solve(i: Int, j: Int, sol: Array<IntArray>, d: IntArray, solvedLine: IntArray): Int {
     if (i < 0 || j < 0) {
         return 0
-    } else if (i == 0 && j == 0) {
+    }
+    if (i == 0 && j == 0) {
         return 1
     }
     if (sol[i][j] != -1) {
-        return sol[i][j]
+        val value = sol[i][j]
+        return value
     }
     sol[i][j] = 0
     if (solve(i - 1, j, sol, d, solvedLine) > 0 && solvedLine[i] != BLACK) {
         updateCellColour(i, WHITE, solvedLine) //i index of cell
-        sol[i][j] = sol[i][j] + solve(i - 1, j, sol, d, solvedLine)
+        val value = sol[i][j]
+        val solved = solve(i - 1, j, sol, d, solvedLine)
+        sol[i][j] = value + solved
     }
     if (solve(i - d[j] - atLeastOneWhiteCell(j), j - 1, sol, d, solvedLine) > 0 && canPlaceBlock(i, j, d, solvedLine)) {
         updateBlockColour(i, j, BLACK, d, solvedLine)
@@ -99,19 +118,22 @@ fun solve(i: Int, j: Int, sol: Array<IntArray>, d: IntArray, solvedLine: IntArra
 }
 
 fun canPlaceBlock(i: Int, j: Int, d: IntArray, solvedLine: IntArray): Boolean {
-    for (m in i.rangeUntil(i - d[j])) {
+    val untilExcluded = i - d[j]
+    for (m in i.rangeUntil(untilExcluded)) {
         if (solvedLine[m] == WHITE) {
             return false
         }
     }
-    if (atLeastOneWhiteCell(j) == 1 && solvedLine[i - d[j]] == BLACK) {
+    if (atLeastOneWhiteCell(j) == 1 && solvedLine[untilExcluded] == BLACK) {
         return false
     }
     return true
 }
 
 fun updateBlockColour(i: Int, j: Int, color: Int, d: IntArray, solvedLine: IntArray) {
-    for (m in i..<(i + d[j])) {
+    val numberOfGears = d[j]
+    val untilExcluded = i - numberOfGears
+    for (m in i.rangeUntil(untilExcluded)) {
         solvedLine[m] = color
     }
     if (atLeastOneWhiteCell(j) == 1) {
